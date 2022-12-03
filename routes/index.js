15280510ace18593f2a1433e168394ffc718970a
quang26071999 <108580841+quang26071999@users.noreply.github.com>
2022-12-03 23:07:13 +0700

    abc

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
-// const insert = require('./insert');
+const insert = require('./insert');
 const Images = require('../model/imageModel');
 
 mongoose.connect('mongodb+srv://quang123:quang123@cluster0.umb30uq.mongodb.net/test');
@@ -19,31 +19,14 @@ router.get('/', (req, res, next) => {
                 if (err) return next(err);
                 res.render('index', {
                     products: products, // sản phẩm trên một page
-                    pages: Math.ceil(count / perPage) // tổng số các page
-                });
-            });
-        });
-});
-
-router.get('/image/:page', (req, res, next) => {
-    let perPage = 6; // số lượng sản phẩm xuất hiện trên 1 page
-    let page = req.params.page ;
-
-    Images
-        .find() // find tất cả các data
-        .skip((perPage * page) - perPage) // Trong page đầu tiên sẽ bỏ qua giá trị là 0
-        .limit(perPage)
-        .exec((err, products) => {
-            Images.countDocuments((err, count) => { // đếm để tính có bao nhiêu trang
-                if (err) return next(err);
-                res.render('index', {
-                    products: products, // sản phẩm trên một page
                     current: page, // page hiện tại
                     pages: Math.ceil(count / perPage) // tổng số các page
                 });
             });
         });
 });
+
+
 router.get('/getImg/:page', (req, res, next) => {
     let perPage = 6; // số lượng sản phẩm xuất hiện trên 1 page
     let page = req.params.page ;
@@ -60,6 +43,6 @@ router.get('/getImg/:page', (req, res, next) => {
         });
 });
 
-
+insert(router);
 
 module.exports = router;
